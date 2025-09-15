8dbea9f8f81320cc8ff0c424b12ee11c382eaff4
stu12286 <fanghang6527@gmail.com>
2025-09-15 16:55:15 +0800

    第六次commit (狀態管理)

--- app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
@@ -16,6 +16,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import tw.edu.pu.csim.tcyang.lotto.ui.theme.LottoTheme
+import androidx.compose.runtime.getValue // 引入 getValue
+import androidx.compose.runtime.mutableStateOf // 引入 mutableStateOf
+import androidx.compose.runtime.remember // 引入 remember
+import androidx.compose.runtime.setValue // 引入 setValue
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,7 +40,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Play(modifier: Modifier = Modifier) {
-    var lucky =(1..100).random()
+    var lucky by remember { mutableStateOf((1..100).random()) }
 
     Column(modifier = modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
